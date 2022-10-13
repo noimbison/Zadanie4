f018e8efa802140759b3489cef810b9828f26a55
Wiktoria <wwiktoria.pawlik@gmail.com>
2022-10-13 23:40:18 +0200

    Initial commit

---
+++ src/pl/vistula/helloworldpawlik/HelloWorldPawlik.java
@@ -0,0 +1,10 @@
+package pl.vistula.helloworldpawlik;
+
+public class HelloWorldPawlik {
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+
+        System.out.println("");
+
+    }
+}
